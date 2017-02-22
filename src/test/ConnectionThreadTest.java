98205a71673ee249be602ee866dd6e71b64cdb75
Evan King <esk79@cornell.edu>
2017-02-21 19:29:35 -0500

    added test stubs

---
+++ src/test/ConnectionThreadTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+/**
+ * Created by EvanKing on 2/21/17.
+ */
+public class ConnectionThreadTest {
+
+    @Test
+    public void testRun() throws Exception {
+
+    }
+
+    @Test
+    public void testSend() throws Exception {
+
+    }
+
+    @Test
+    public void testReceive() throws Exception {
+
+    }
+
+    @Test
+    public void testClose() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
