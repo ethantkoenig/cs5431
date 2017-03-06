6425968bd9ba780347cf5140692ab5a0a70ff60b
Ethan Koenig <ethantkoenig@gmail.com>
2017-03-06 13:44:47 -0500

    Unit tests for MinerThread

---
+++ src/test/java/network/MinerThreadTest.java
@@ -0,0 +1,34 @@
+package network;
+
+import block.Block;
+import org.junit.Assert;
+import org.junit.Test;
+import testutils.RandomizedTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class MinerThreadTest extends RandomizedTest {
+
+    @Test
+    public void test() throws Exception {
+        Block block = randomBlock(randomShaTwoFiftySix());
+        BlockingQueue<Message> queue = new ArrayBlockingQueue<>(5);
+        MinerThread minerThread = new MinerThread(block, queue);
+
+        minerThread.start();
+        Message msg = queue.take();
+        Assert.assertEquals(errorMessage, msg.type, Message.BLOCK);
+
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        block.serialize(new DataOutputStream(outputStream));
+        Assert.assertTrue(errorMessage, block.checkHash());
+
+        Assert.assertArrayEquals(errorMessage,
+                msg.payload,
+                outputStream.toByteArray());
+    }
+}
