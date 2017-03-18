d281f51a6ef8aa47befdca1ce354c7cd360915e7
Ethan Koenig <ethantkoenig@gmail.com>
2017-03-18 19:22:43 -0400

    Proof-of-concept for webapp transactions

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -2,7 +2,11 @@ package server;
 
 import server.config.DatabaseConfig;
 import server.controllers.IndexController;
+import server.controllers.TransactionController;
 import server.controllers.UserController;
+import server.utils.Constants;
+
+import java.net.InetSocketAddress;
 
 import static spark.Spark.port;
 import static spark.Spark.staticFiles;
@@ -10,7 +14,8 @@ import static spark.Spark.staticFiles;
 
 public class Application {
 
-    public static void run() {
+    public static void run(InetSocketAddress nodeAddress) {
+        Constants.setNodeAddress(nodeAddress);
 
         // Configure Spark on port 5000
         port(5000);
@@ -22,5 +27,6 @@ public class Application {
         DatabaseConfig.dbInit();
         IndexController.serveIndexPage();
         UserController.startUserController();
+        TransactionController.makeTransaction();
     }
 }
