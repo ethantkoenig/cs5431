bbd0ba61cc79fcb410f90b468d963972ccc34b10
Ethan Koenig <ethantkoenig@gmail.com>
2017-03-19 13:29:59 -0400

    Config class for constants, can alter for tests

--- src/test/java/network/MinerTest.java
+++ src/test/java/network/MinerTest.java
@@ -11,6 +11,7 @@ import transaction.Transaction;
 import transaction.TxIn;
 import transaction.TxOut;
 import utils.ByteUtil;
+import utils.Config;
 import utils.Crypto;
 
 import java.io.IOException;
@@ -27,6 +28,8 @@ public class MinerTest extends RandomizedTest {
 
     @Test
     public void testRun() throws Exception {
+        Config.HASH_GOAL.set(1);
+
         KeyPair pair1 = randomKeyPair();
         KeyPair pair2 = randomKeyPair();
 
