94ea212d3eea3157441253ac09355f01f81d2ed0
eperdew <eperdew17@gmail.com>
2017-03-23 05:53:33 -0400

    Change KeyPair generator to use InsecureSecureRandom

--- src/test/java/generators/model/SigningKeyPairGenerator.java
+++ src/test/java/generators/model/SigningKeyPairGenerator.java
@@ -3,10 +3,15 @@ package generators.model;
 import com.pholser.junit.quickcheck.generator.GenerationStatus;
 import com.pholser.junit.quickcheck.generator.Generator;
 import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import testutils.InsecureSecureRandom;
+import testutils.SeededRandom;
 import utils.Crypto;
 
-import java.security.GeneralSecurityException;
-import java.security.KeyPair;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.Random;
 
 public class SigningKeyPairGenerator extends Generator<KeyPair> {
 
@@ -16,16 +21,33 @@ public class SigningKeyPairGenerator extends Generator<KeyPair> {
         super(KeyPair.class);
     }
 
+    @BeforeClass
+    public static void setupClass() {
+        Crypto.init();
+    }
+
     @Override
     public KeyPair generate(SourceOfRandomness random, GenerationStatus status) {
-        for (int i = 0; i < MAX_TRIES; ++i) {
-            try {
-                // TODO: incorporate SourceOfRandomness, handle crypto.init()
-                return Crypto.signatureKeyPair();
-            } catch (GeneralSecurityException e) {
-                // try again
-            }
+        SeededRandom seededRandom = SeededRandom.fixedSeed(random.nextLong());
+        Random rand = seededRandom.random();
+
+        KeyPairGenerator generator = null;
+        try {
+            generator = KeyPairGenerator.getInstance("ECDSA", "BC");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
         }
-        return null;
+
+        ECGenParameterSpec ecSpec = new ECGenParameterSpec("P-256");
+        SecureRandom secureRandom = new InsecureSecureRandom(rand);
+        try {
+            generator.initialize(ecSpec, secureRandom);
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+
+        return generator.generateKeyPair();
     }
 }
