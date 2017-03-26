5cffeb16f924020bd144bb8083d2191a8be60fa9
eperdew <eperdew17@gmail.com>
2017-03-26 10:22:46 -0400

    Update BlockChain tests to use an easier difficulty

--- src/test/java/block/BlockChainProperties.java
+++ src/test/java/block/BlockChainProperties.java
@@ -2,8 +2,10 @@ package block;
 
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import utils.Config;
 import utils.Crypto;
 
 @RunWith(JUnitQuickcheck.class)
@@ -12,6 +14,12 @@ public class BlockChainProperties {
     @BeforeClass
     public static void initCrypto() {
         Crypto.init();
+    }
+
+    @Before
+    public void setConfig() {
+        Config.HASH_GOAL.set(1);
+
     }
 
     @Property(trials = 1)
