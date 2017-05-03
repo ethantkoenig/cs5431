377fd429d8e1f988fa21cfe64b2a5a7b0c7c2376
Eric Perdew <eperdew17@gmail.com>
2017-05-03 09:25:23 -0400

    Suppress a findbugs false positive due to fields being written only by JCommander

--- src/main/java/jcommander/CommandMiner.java
+++ src/main/java/jcommander/CommandMiner.java
@@ -2,7 +2,12 @@ package jcommander;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import edu.umd.cs.findbugs.annotations.SuppressWarnings;
 
+@SuppressWarnings(
+        value="UWF_NULL_FIELD",
+        justification="Field is written by JCommander, which is opaque to findbugs"
+)
 @Parameters(commandDescription = "Run a mining node in the network")
 public class CommandMiner {
 
